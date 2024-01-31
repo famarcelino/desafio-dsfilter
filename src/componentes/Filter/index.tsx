97ba94312e4f82b69cc819bccfe2c8c8a8ae3960
Francisco de Assis Marcelino <famarcelino@gmail.com>
2024-01-31 09:52:21 -0300

    Event onFilter

--- src/componentes/Filter/index.tsx
+++ src/componentes/Filter/index.tsx
@@ -1,9 +1,10 @@
 import './styles.css';
 
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import * as productService from '../../services/product-service';
 import Listing from '../Listing';
 import { ContextProductCount } from '../../utils/context-product';
+import { ProductDTO } from '../../models/product';
 
 type FormData = {
     minPrice?: number;
@@ -13,34 +14,44 @@ type FormData = {
 
 export default function Filter() {
 
-    const {setContextProductCount} = useContext(ContextProductCount);
+    const { setContextProductCount } = useContext(ContextProductCount);
+
+    const [products, setProducts] = useState<ProductDTO[]>([]);
 
     const [formData, setFormData] = useState<FormData>({
         minPrice: undefined,
         maxPrice: undefined
     });
 
-    let lenProd = productService.findByPrice(formData.minPrice || 0, formData.maxPrice ||Number.MAX_VALUE).map(product => <Listing key={product.id} product={product} />).length;
+    useEffect(() => {
+        const listProd = productService.findByPrice(0, Number.MAX_VALUE);
+        setProducts(listProd);
+        setContextProductCount(listProd.length);
+    }, [formData]);
+
 
     function handleInputChange(event: any) {
         const value = event.target.value;
         const name = event.target.name;
         setFormData({ ...formData, [name]: value });
-        
+
     }
 
-    function handleSubmit(event: any) {
+    function handleOnSubmit(event: any) {
         event.preventDefault();
-        lenProd = productService.findByPrice(formData.minPrice || 0, formData.maxPrice || Number.MAX_VALUE).map(product => <Listing key={product.id} product={product} />).length;
-        setContextProductCount(lenProd);
+        onFilter(Number(formData.minPrice), Number(formData.maxPrice));
     }
 
-    setContextProductCount(lenProd);
+    function onFilter(min: number, max: number) {
+        const listProd = productService.findByPrice(min, max);
+        setProducts(listProd);
+        setContextProductCount(listProd.length);
+    }
 
     return (
         <>
             <div className='container ftr-container mt20'>
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handleOnSubmit}>
                     <div className='mb20'>
                         <input
                             name="minPrice"
@@ -59,12 +70,14 @@ export default function Filter() {
                             onChange={handleInputChange}
                         />
                     </div>
-                    <button type="submit" className='mb10'>Filtrar</button>
+                    <button type="submit" className='mb10' >Filtrar</button>
                 </form>
             </div>
             <div className='container ftr-container mt20'>
                 {
-                    productService.findByPrice(formData.minPrice || 0, formData.maxPrice || Number.MAX_VALUE).map(product => <Listing key={product.id} product={product} />)
+                    products.map(
+                        product => <Listing key={product.id} product={product} />
+                    )
                 }
             </div>
         </>
--- src/componentes/NumberInfo/index.tsx
+++ src/componentes/NumberInfo/index.tsx
@@ -2,7 +2,6 @@ import { useContext } from "react";
 import { ContextProductCount } from "../../utils/context-product";
 
 export default function NumberInfo() {
-    
     const { contextProductCount } = useContext(ContextProductCount);
 
     return (
